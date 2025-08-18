625ac267c8deca641435c6288cb57937964cc286
Rajeev Tapadia <rtapadia730@gmail.com>
2025-08-18 12:44:28 +0530

    updated gnome-terminal backup script to use Ptyxis
    Signed-off-by: Rajeev Tapadia <rtapadia730@gmail.com>

--- gnome-terminal/terminal-profile.sh
+++ gnome-terminal/terminal-profile.sh
@@ -5,13 +5,13 @@
 set -e
 
 if [[ $1 == 'backup' ]]; then
-  dconf dump '/org/gnome/terminal/legacy/profiles:/' > gnome-terminal.dconf
+  dconf dump '/org/gnome/Ptyxis/' > gnome-terminal/gnome-terminal.dconf
   echo "backup done"
   exit 0
 fi
 if [[ $1 == 'restore' ]]; then
-  dconf reset -f '/org/gnome/terminal/legacy/profiles:/'
-  dconf load '/org/gnome/terminal/legacy/profiles:/' < gnome-terminal.dconf
+  dconf reset -f '/org/gnome/Ptyxis/'
+  dconf load '/org/gnome/Ptyxis/' < gnome-terminal/gnome-terminal.dconf
   echo "restore done"
   exit 0
 fi
